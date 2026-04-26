e645f81138d9f578d8785d523f0ea45f18d4daef
hadzikk <hadzikmochammad@gmail.com>
2026-04-26 10:47:46 +0700

    feat: configuring mongo connection to serverless mode

--- src/lib/mongo.js
+++ src/lib/mongo.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose')
 const config = require('../config')
 const MONGO_URI = config.MONGO_URI
 
+let cachedConnection = null
+
 const mongodbConnect = async () => {
+    if (cachedConnection) return cachedConnection
+
+    const options = {
+        bufferCommands: false,
+        connectTimeoutMS: 10000
+    }
+
     try {
-        await mongoose.connect(MONGO_URI)
+        const connection = await mongoose.connect(MONGO_URI, options)
         console.log("Connected to MongoDB successfully")
+        return connection
     } catch (error) {
         console.error("Error connecting to MongoDB:", error.message)
         process.exit(1)
